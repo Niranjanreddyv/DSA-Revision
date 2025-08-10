dcf23e70d8f9348d526ed928e0214503268e1cca
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-10 07:40:56 +0530

    pointers basics add

--- Pointers/basics1.cpp
+++ Pointers/basics1.cpp
@@ -4,10 +4,19 @@ int main(){
     int a = 10;
     cout<<&a<<endl;
     int *ptr = &a;
-    cout<<ptr<<endl;
+    cout<<ptr<<endl; // address of a
+    cout<<*ptr<<endl; // value of a
+    cout<<sizeof(ptr)<<endl;
+
+    int b = 20;
+    ptr = &b;
+    cout<<*ptr<<endl; // * dereferencing operator
+
     float m = 7.8;
     float *ptr2 = &m;
-    cout<<ptr<<endl;
+    cout<<ptr2<<endl;
+    cout<<*ptr2<<endl;
+    cout<<sizeof(ptr2)<<endl;
 
     return 0;
 }
