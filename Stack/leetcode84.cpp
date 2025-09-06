f7c8a114fe5cf45bfccbfc8ed13f87f967d4a438
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-09-06 17:18:15 +0530

    largest rectangle in a histogram code add

---
+++ Stack/leetcode84.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    int n = heights.size();
+    stack<int> st;
+    int ans = 0;
+    int index;
+    for(int i = 0;i<n;i++){
+        while(!st.empty() && heights[st.top()] > heights[i]){
+            index = st.top();
+            st.pop();
+            if(!st.empty()){
+                ans = max(ans , heights[index] * (i  - st.top() - 1));
+            }else{
+                ans = max(ans , heights[index] * i);
+            }
+        }
+        st.push(i);
+
+    }
+
+    while(!st.empty()){
+        index = st.top();
+        st.pop();
+        if(!st.empty()){
+            ans = max(ans , heights[index] * (n - st.top() - 1));
+        }else{
+            ans = max(ans , heights[index] * n);
+        }
+    }
+
+    return ans;
+    
+}
+
+int main(){
+    vector<int> arr = {2,1,5,6,2,3};
+    cout<<largestRectangleArea(arr);
+    return 0;
+}
